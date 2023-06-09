52e9a1ee6d641cd61b66fe83bc8ac9c3962daa2d
Matheus Giraldi <matheushenriquegiraldi@gmail.com>
2023-06-09 16:44:28 -0300

    App Xamarin

---
+++ App_Xamarin/App_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace App_Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void GerarEmailClicked(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text.Trim();
+            string sobrenome = txtSobrenome.Text.Trim();
+
+            string email = $"{sobrenome.ToLower()}{nome.ToLower()}@ufn.edu.br";
+
+            lblEmail.Text = email;
+        }
+        
+        private async void IrParaPage1Clicked(object sender, EventArgs e)
+{
+            await Navigation.PushAsync(new NavigationPage(new Page1()));
+        }
+
+
+
+    }
+}
---
+++ App_Xamarin/App_Xamarin/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace App_Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void CalcularIMC_Clicked(object sender, EventArgs e)
+        {
+            // Obter os valores dos Entry
+            string nome = txtNome.Text;
+            int idade = Convert.ToInt32(txtIdade.Text);
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+
+            // Calcular o IMC
+            double imc = peso / (altura * altura);
+
+            // Exibir o resultado
+            lblIMC.Text = $"IMC de {nome}: {imc:F2}";
+        }
+    }
+
+}
